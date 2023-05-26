792416cd94b296d0b76a6c8fddb86d629baa453d
iamDrakkir <Hagelin.Rickard@gmail.com>
2023-05-26 07:37:33 +0200

    revert telescope changes

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -11,172 +11,36 @@ return {
   cmd = "Telescope",
   keys = {
     -- find/files
-    {
-      "<leader>ff",
-      "<cmd>Telescope find_files<cr>",
-      desc =
-      "Find Files (root dir)"
-    },
-    {
-      "<leader>fb",
-      "<cmd>Telescope buffers ignore_current_buffer=true<cr>",
-      desc =
-      "Switch Buffer"
-    },
-    {
-      "<leader>,",
-      "<cmd>Telescope buffers ignore_current_buffer=true<cr>",
-      desc =
-      "Switch Buffer"
-    },
-    {
-      "<Leader>fe",
-      "<cmd>Telescope file_browser path=%:p:h<cr>",
-      desc =
-      "Explore Files (cwd)"
-    },
-    {
-      "<Leader>fE",
-      "<cmd>Telescope file_browser<cr>",
-      desc =
-      "Explore Files (root dir)"
-    },
-    {
-      "<Leader>fr",
-      "<cmd>Telescope repo<cr>",
-      desc =
-      "Find Repos"
-    },
-    {
-      "<leader>fo",
-      "<cmd>Telescope oldfiles<cr>",
-      desc =
-      "Old files"
-    },
-    -- {
-    --   "<leader>/",
-    --   "<cmd>Telescope live_grep<cr>",
-    --   desc =
-    --   "Find in Files (Grep)"
-    -- },
-    {
-      '<leader>/',
-      "<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown { winblend = 10, previewer = false })<cr>",
-      desc =
-      '[/] Fuzzily search in current buffer'
-    },
+    { "<leader>ff", "<cmd>Telescope find_files<cr>",                         desc = "Find Files (root dir)" },
+    { "<leader>fb", "<cmd>Telescope buffers ignore_current_buffer=true<cr>", desc = "Switch Buffer" },
+    { "<leader>,",  "<cmd>Telescope buffers ignore_current_buffer=true<cr>", desc = "Switch Buffer" },
+    { "<Leader>fe", "<cmd>Telescope file_browser path=%:p:h<cr>",            desc = "Explore Files (cwd)" },
+    { "<Leader>fE", "<cmd>Telescope file_browser<cr>",                       desc = "Explore Files (root dir)" },
+    { "<Leader>fr", "<cmd>Telescope repo<cr>",                               desc = "Find Repos" },
+    { "<leader>fo", "<cmd>Telescope oldfiles<cr>",                           desc = "Old files" },
+    { "<leader>/",  "<cmd>Telescope live_grep<cr>",                          desc = "Find in Files (Grep)" },
     -- git
-    {
-      "<leader>gc",
-      "<cmd>Telescope git_commits<cr>",
-      desc =
-      "commits"
-    },
-    {
-      "<leader>gs",
-      "<cmd>Telescope git_status<cr>",
-      desc =
-      "status"
-    },
+    { "<leader>gc", "<cmd>Telescope git_commits<cr>",                        desc = "commits" },
+    { "<leader>gs", "<cmd>Telescope git_status<cr>",                         desc = "status" },
     -- search
-    {
-      "<leader>sa",
-      "<cmd>Telescope autocommands<cr>",
-      desc =
-      "Auto Commands"
-    },
-    {
-      "<leader>sb",
-      "<cmd>Telescope current_buffer_fuzzy_find<cr>",
-      desc =
-      "Buffer"
-    },
-    {
-      "<leader>sc",
-      "<cmd>Telescope command_history<cr>",
-      desc =
-      "Command History"
-    },
-    {
-      "<leader>s:",
-      "<cmd>Telescope command_history<cr>",
-      desc =
-      "Command History"
-    },
-    {
-      "<leader>sC",
-      "<cmd>Telescope commands<cr>",
-      desc =
-      "Commands"
-    },
-    {
-      "<leader>sd",
-      "<cmd>Telescope diagnostics<cr>",
-      desc =
-      "Diagnostics"
-    },
-    {
-      "<leader>sg",
-      "<cmd>Telescope live_grep<cr>",
-      desc =
-      "Grep (root dir)"
-    },
+    { "<leader>sa", "<cmd>Telescope autocommands<cr>",                       desc = "Auto Commands" },
+    { "<leader>sb", "<cmd>Telescope current_buffer_fuzzy_find<cr>",          desc = "Buffer" },
+    { "<leader>sc", "<cmd>Telescope command_history<cr>",                    desc = "Command History" },
+    { "<leader>s:", "<cmd>Telescope command_history<cr>",                    desc = "Command History" },
+    { "<leader>sC", "<cmd>Telescope commands<cr>",                           desc = "Commands" },
+    { "<leader>sd", "<cmd>Telescope diagnostics<cr>",                        desc = "Diagnostics" },
+    { "<leader>sg", "<cmd>Telescope live_grep<cr>",                          desc = "Grep (root dir)" },
     -- { "<leader>sG", "<cmd>Telescope live_grep cwd=false<cr>", desc = "Grep (cwd)" },
-    {
-      "<leader>sh",
-      "<cmd>Telescope help_tags<cr>",
-      desc =
-      "Help Pages"
-    },
-    {
-      "<leader>sH",
-      "<cmd>Telescope highlights<cr>",
-      desc =
-      "Search Highlight Groups"
-    },
-    {
-      "<leader>sk",
-      "<cmd>Telescope keymaps<cr>",
-      desc =
-      "Key Maps"
-    },
-    {
-      "<leader>sM",
-      "<cmd>Telescope man_pages<cr>",
-      desc =
-      "Man Pages"
-    },
-    {
-      "<leader>sm",
-      "<cmd>Telescope marks<cr>",
-      desc =
-      "Jump to Mark"
-    },
-    {
-      "<leader>so",
-      "<cmd>Telescope vim_options<cr>",
-      desc =
-      "Options"
-    },
-    {
-      "<leader>sw",
-      "<cmd>Telescope grep_string<cr>",
-      desc =
-      "Word (root dir)"
-    },
+    { "<leader>sh", "<cmd>Telescope help_tags<cr>",                          desc = "Help Pages" },
+    { "<leader>sH", "<cmd>Telescope highlights<cr>",                         desc = "Search Highlight Groups" },
+    { "<leader>sk", "<cmd>Telescope keymaps<cr>",                            desc = "Key Maps" },
+    { "<leader>sM", "<cmd>Telescope man_pages<cr>",                          desc = "Man Pages" },
+    { "<leader>sm", "<cmd>Telescope marks<cr>",                              desc = "Jump to Mark" },
+    { "<leader>so", "<cmd>Telescope vim_options<cr>",                        desc = "Options" },
+    { "<leader>sw", "<cmd>Telescope grep_string<cr>",                        desc = "Word (root dir)" },
     -- { "<leader>sW", "<cmd>Telescope grep_string cwd=false<cr>", desc = "Word (cwd)" },
-    {
-      "<leader>uC",
-      "<cmd>Telescope colorscheme enable_preview=true<cr>",
-      desc =
-      "Colorscheme with preview"
-    },
-    {
-      "<leader>fu",
-      "<cmd>Telescope undo<cr>",
-      desc =
-      "Undo tree"
-    },
+    { "<leader>uC", "<cmd>Telescope colorscheme enable_preview=true<cr>",    desc = "Colorscheme with preview" },
+    { "<leader>fu", "<cmd>Telescope undo<cr>",                               desc = "Undo tree" },
     -- {
     --   "<leader>ss",
     --   Util.telescope("lsp_document_symbols", {
