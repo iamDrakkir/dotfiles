28d2e1433fef4dc5529c44d7c09e81b2d7490e2c
iamDrakkir <Hagelin.Rickard@gmail.com>
2023-05-29 19:37:29 +0200

    zsh state

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -3,6 +3,7 @@
 export XDG_CONFIG_HOME=$HOME/.config
 export XDG_CACHE_HOME=$HOME/.cache
 export XDG_DATA_HOME=$HOME/.local/share
+export XDG_STATE_HOME=$HOME/.local/state
 HISTSIZE=100000
 SAVEHIST=100000
 
