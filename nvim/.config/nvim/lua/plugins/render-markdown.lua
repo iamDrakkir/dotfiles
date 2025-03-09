2ea3c3ea32938b138f993e3b02ee330db07cb020
iamDrakkir <Hagelin.Rickard@gmail.com>
2025-03-09 21:10:29 +0100

    better markdown render

--- nvim/.config/nvim/lua/plugins/render-markdown.lua
+++ nvim/.config/nvim/lua/plugins/render-markdown.lua
@@ -1,8 +1,21 @@
 return {
   "MeanderingProgrammer/render-markdown.nvim",
+  enabled = true,
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
     "nvim-tree/nvim-web-devicons"
   },
   ft = { "markdown" },
+  opts = {
+    code = {
+      width = 'block',
+      left_pad = 2,
+      right_pad = 4,
+    },
+    render_modes = true,
+    heading = {
+      width = "block",
+    },
+    pipe_table = { preset = 'round' },
+  },
 }
